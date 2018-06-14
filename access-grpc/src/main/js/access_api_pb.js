c6904c8f0fc83b9b9b3007251b14b95a0a8ff012
theborakompanioni <theborakompanioni+github@gmail.com>
2018-06-14 22:28:12 +0200

    enable protobuf js plugin

---
+++ access-grpc/src/main/js/access_api_pb.js
@@ -0,0 +1,18 @@
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+
+var jspb = require('google-protobuf');
+var goog = jspb;
+var global = Function('return this')();
+
+var access_api_access_pb = require('./access_api_access_pb.js');
+var access_api_auth_pb = require('./access_api_auth_pb.js');
+var access_api_device_pb = require('./access_api_device_pb.js');
+var access_api_vehicle_pb = require('./access_api_vehicle_pb.js');
+goog.object.extend(exports, proto.amv_access_api);
